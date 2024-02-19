b627461cccae6941dffceb490ebdd4882fd95f80
chanyoung <chanchany0112@gmail.com>
2024-02-19 17:24:43 +0900

    [modified] modify test_compute_candidate to vector

--- test_retrieval.py
+++ test_retrieval.py
@@ -1,6 +1,9 @@
+import os
+from PIL import Image
 import torch
 import torchvision.models as models
 import torch.nn as nn
+import torchvision.transforms as transforms
 from myexception import RetrievalException
 from myerror import RetrievalErrorCode
 # --- setup ---
@@ -64,12 +67,42 @@ def test_no_model_name():
 
 
 # # ---- test vectorize candidates ----
+# 딕셔너리 저장 {'주소' : 벡터}
 # # given : candidate images
 # # when : after loading model
 # # then : get list vectors of candidate images
-# def test_vectorize_candidate():
+image_dir = "./sample_image"
+image_dict = dict()
 
-#     assert 1
+# 이미지를 모델 입력에 맞게 전처리
+preprocess = transforms.Compose([
+    transforms.Resize((512, 512)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+
+def test_vectorize_candidate():
+    model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
+    model = torch.nn.Sequential(*list(model.children())[:-1])
+
+    # 디렉토리 내의 모든 파일에 대해 반복
+    for file_path in sorted(os.listdir(image_dir)):
+        # 파일의 확장자가 이미지인지 확인
+        # print(file_path)
+        if file_path.endswith(".jpg") or file_path.endswith(".jpeg"):
+            input_image = Image.open(os.path.join(image_dir, file_path))
+            input_image = input_image.convert("RGB")
+            input_tensor = preprocess(input_image)
+            input_batch = input_tensor.unsqueeze(0)  # 배치 차원 추가
+            model.eval()
+            with torch.no_grad():
+                feature_vector = model(input_batch)
+                image_dict[file_path] = feature_vector
+    # print(image_dict)
+    print(image_dict[file_path].size())
+
+    assert image_dict[file_path].size() == torch.Size([1, 2048, 1, 1])
 
 
 # def test_no_folder_dir():
@@ -78,11 +111,6 @@ def test_no_model_name():
 
 
 # def test_no_images_in_folder():
-
-#     assert 1
-
-
-# def test_not_jpg_format():
 
 #     assert 1
 
